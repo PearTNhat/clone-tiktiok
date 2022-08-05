6c0edf51f56af785d038656af697fe4d4414c0fd
PearTNhat <99273772+PearTNhat@users.noreply.github.com>
2022-08-05 17:24:30 +0700

    xay dựng layout

--- src/component/Layout/DefaultLayout/Sidebar/index.js
+++ src/component/Layout/DefaultLayout/Sidebar/index.js
@@ -1,5 +1,8 @@
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+const cx = classNames.bind(styles);
 function Sidebar() {
-    return <h2>Sidebar</h2>;
+    return <div className={cx('container')}>sidebar</div>;
 }
 
 export default Sidebar;
