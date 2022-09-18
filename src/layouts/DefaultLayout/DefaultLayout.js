221340e0d11568b29784d113be79694f21426278
Tuan Nhat <letuannhat105@gmail.com>
2022-09-18 22:19:44 +0700

    ###

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -1,4 +1,4 @@
-import Header from '../Components/Header';
+import Header from '../components/Header';
 import Sidebar from './Sidebar';
 import classNames from 'classnames/bind';
 import styles from './DefaultLayout.module.scss';
